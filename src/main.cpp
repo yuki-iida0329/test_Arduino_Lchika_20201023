575f8aaedcb58a982a01877010ea1c1c900298a9
yuki.iida <yuki.iida@optim.co.jp>
2020-12-03 14:05:55 +0900

    テストコミット

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  delay(1000);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  delay(200);
+  digitalWrite(13, LOW);
+  delay(200);
+}
